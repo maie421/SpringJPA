50a90211d153e523da6aaf851c41b397a8aed545
maie421 <maie421@naver.com>
2020-10-31 20:28:00 +0900

    WEB UI

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,10 +1,14 @@
 package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderSearch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -19,4 +23,10 @@ public class OrderRepository {
     public Order findOne(Long id){
         return em.find(Order.class,id);
     }
+
+    public List<Order> findAll(OrderSearch orderSearch) {
+        //... 검색 로직
+        return null;
+    }
+
 }
